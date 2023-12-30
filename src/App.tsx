829b43840b8f6258de3b3c40a4fa56a88b08d7f9
Mohammed Afsal <mohammedafsaltut@gmail.com>
2023-12-30 13:16:29 +0530

    Add types for notes and tags

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,31 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import NoteList from "./pages/NoteList";
 import NewNote from "./pages/NewNote";
 
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tag: Tag[];
+};
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
 const App: React.FC = () => {
   return (
     <Container className="my-4">
