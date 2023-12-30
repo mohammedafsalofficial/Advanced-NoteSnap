cda5d698392ae02f250211cc398bbea39b44e0ed
Mohammed Afsal <mohammedafsaltut@gmail.com>
2023-12-30 11:59:32 +0530

    Add Route for NewNote page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import NoteList from "./pages/NoteList";
+import NewNote from "./pages/NewNote";
 
 const App: React.FC = () => {
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<NoteList />} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Container>
   );
