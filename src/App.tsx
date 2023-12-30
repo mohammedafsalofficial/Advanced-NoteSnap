883f5ec0d6d73c7db27ce2a72a28b9908733038c
Mohammed Afsal <mohammedafsaltut@gmail.com>
2023-12-30 11:56:42 +0530

    Add route for NoteList page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import NoteList from "./pages/NoteList";
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<NoteList />} />
+      </Routes>
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/pages/NoteList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NoteList: React.FC = () => {
+  return <div>NoteList</div>;
+};
+
+export default NoteList;
